47529a5a4b785e8eab834d0be134f1a0c4e9deda
Joe Sill <jsill@alumni.caltech.edu>
2025-10-04 12:48:06 +0000

    more refactoring

--- cs336_basics/llModel.py
+++ cs336_basics/llModel.py
@@ -22,6 +22,50 @@ class LLModel(torch.nn.Module):
         self.rope_theta=rope_theta,
         #weights: dict[str, Tensor],
 
+
+    def run_embedding(
+            vocab_size: int,
+            d_model: int,
+            weights: Float[Tensor, " vocab_size d_model"],
+            token_ids: Int[Tensor, " ..."],
+    ) -> Float[Tensor, " ... d_model"]:
+        return weights[token_ids,:]
+
+    def run_swiglu(
+            d_model: int,
+            d_ff: int,
+            w1_weight: Float[Tensor, " d_ff d_model"],
+            w2_weight: Float[Tensor, " d_model d_ff"],
+            w3_weight: Float[Tensor, " d_ff d_model"],
+            in_features: Float[Tensor, " ... d_model"],
+    ) -> Float[Tensor, " ... d_model"]:
+        sigm=torch.nn.Sigmoid()
+        xw=in_features@torch.transpose(w1_weight,0,1)
+        swish=np.multiply(xw,sigm(xw))
+        xv=in_features@torch.transpose(w3_weight,0,1)
+        swishTimesXV=np.multiply(swish,xv)
+        return swishTimesXV@torch.transpose(w2_weight,0,1)
+
+    
+    def run_rmsnorm(d_model: int,
+                    eps: float,
+                    weights: Float[Tensor, " d_model"],
+                    in_features: Float[Tensor, " ... d_model"],
+                    ) -> Float[Tensor, " ... d_model"]:
+            a=in_features
+            aSq=a.square()
+            dim=a.dim()
+            aSqMean=torch.mean(aSq,dim-1)#,tuple(range(0,dim-1)))                                                                                        
+            aRMS=aSqMean.sqrt()+ eps
+
+            #REVISIT...there is surely a better way than all this transposing                                                                             
+            a=a.transpose(0,2)
+            a=a.transpose(1,2)
+            aDiv=a.div(aRMS)
+            aDiv=aDiv.transpose(1,2)
+            aDiv=aDiv.transpose(0,2)
+            return aDiv*weights
+        
     def run_scaled_dot_product_attention(
             Q: Float[Tensor, " ... queries d_k"],
             K: Float[Tensor, " ... keys d_k"],
@@ -153,3 +197,93 @@ class LLModel(torch.nn.Module):
                 res[:,i,:]=in_query_or_key[:,i,:]@ropeMat(token_positions[i],d_k)
 
         return res
+
+        seqLen=in_features.shape[1]
+
+
+    def run_transformer_block(
+            d_model: int,
+            num_heads: int,
+            d_ff: int,
+            max_seq_len: int,
+            theta: float,
+            weights: dict[str, Tensor],
+            in_features: Float[Tensor, " batch sequence_length d_model"],
+    ) -> Float[Tensor, " batch sequence_length d_model"]:
+
+        seqLen=in_features.shape[1]
+        tokenPos=torch.Tensor([[i for i in range(0,seqLen )]])
+
+        eps=5e-6
+        rmsNormOut=LLModel.run_rmsnorm(d_model,eps,weights['ln1.weight'],in_features)
+
+        attnOut=LLModel.run_multihead_self_attention_with_rope(d_model,num_heads,
+                                                               max_seq_len,
+                                                               theta,
+                                                               weights['attn.q_proj.weight'],
+                                                               weights['attn.k_proj.weight'],
+                                                               weights['attn.v_proj.weight'],
+                                                               weights['attn.output_proj.weight'],
+                                                               rmsNormOut,#in_features,                                                 
+                                                               tokenPos)
+
+
+        rmsNormOut2=LLModel.run_rmsnorm(d_model,eps,weights['ln2.weight'],
+                                        attnOut + in_features)
+
+        d_ff=weights['ffn.w1.weight'].shape[0]
+
+        swigluOut=LLModel.run_swiglu(d_model,d_ff,weights['ffn.w1.weight'],
+                                     weights['ffn.w2.weight'],
+                                     weights['ffn.w3.weight'],
+                                     rmsNormOut2)
+
+
+        return swigluOut + attnOut + in_features
+
+
+    def run_transformer_lm(
+            vocab_size: int,
+            context_length: int,
+            d_model: int,
+            num_layers: int,
+            num_heads: int,
+            d_ff: int,
+            rope_theta: float,
+            weights: dict[str, Tensor],
+            in_indices: Int[Tensor, " batch_size sequence_length"],
+    ) -> Float[Tensor, " batch_size sequence_length vocab_size"]:
+        layerOutput=LLModel.run_embedding(vocab_size,d_model,weights['token_embeddings.weight'],
+                                          in_indices)
+
+
+
+        for i in range(num_layers):
+            weightDct=dict()
+            for ky in ['attn.q_proj.weight',
+                       'attn.k_proj.weight',
+                       'attn.v_proj.weight',
+                       'attn.output_proj.weight',
+                       'ln1.weight',
+                       'ln2.weight',
+                       'ffn.w1.weight',
+                       'ffn.w2.weight',
+                       'ffn.w3.weight']:
+                weightDct[ky]=weights['layers.%d.%s'%(i,ky)]
+
+            layerOutput=LLModel.run_transformer_block(d_model,
+                                                      num_heads,
+                                                      d_ff,
+                                                      context_length,#???                                                               
+                                                      rope_theta,
+                                                      weightDct,
+                                                      layerOutput)#????                                                                      
+
+
+        eps=5e-6
+        normed=LLModel.run_rmsnorm(d_model,eps,weights['ln_final.weight'],layerOutput)
+        finalOut=normed@torch.transpose(weights['lm_head.weight'],0,1)
+
+        return finalOut
+
+    
