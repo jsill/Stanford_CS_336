9e942a8754b1b29aef33cdc1b1349eb0e9c54886
Joe Sill <jsill@alumni.caltech.edu>
2025-10-09 20:31:50 +0000

    moved train_bpe

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -5,7 +5,18 @@ from typing import IO, Any, BinaryIO
 
 import regex as re
 
-import numpy
+import numpy as np
+
+import collections
+from collections import Counter
+
+from multiprocessing import Process
+
+from functools import cmp_to_key
+
+import bisect
+
+import pickle
 
 pat_str=r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
@@ -63,20 +74,24 @@ def find_chunk_boundaries(
     # Get total file size in bytes                                                                                                                 
     file.seek(0, os.SEEK_END)
     file_size = file.tell()
+    #print('file_size in here is',file_size)
     file.seek(0)
 
     chunk_size = file_size // desired_num_chunks
 
-    print('chunk_size in here is',chunk_size)
+    #print('chunk_size in here is',chunk_size)
     # Initial guesses for chunk boundary locations, uniformly spaced                                                                               
     # Chunks start on previous index, don't include last index                                                                                     
     chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
     chunk_boundaries[-1] = file_size
 
+    #print('init boundaries',chunk_boundaries)
     mini_chunk_size = 4096  # Read ahead by 4k bytes at a time                                                                                     
 
     for bi in range(1, len(chunk_boundaries) - 1):
         initial_position = chunk_boundaries[bi]
+        #print('initial position',initial_position)
+        #print('cb now',chunk_boundaries)
         file.seek(initial_position)  # Start at boundary guess                                                                                     
         while True:
             mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk                                                                           
@@ -366,3 +381,281 @@ class Tokenizer:
         return ret
 
     
+def train_bpe(input_path: str | os.PathLike,
+    vocab_size: int,
+    special_tokens: list[str],
+    **kwargs,
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+
+    import datetime
+    print('at start',datetime.datetime.now())
+
+    vocab={x:bytes([x]) for x in range(256)}
+
+    currVocabSize=len(vocab)
+    for sTok in special_tokens:
+        vocab[currVocabSize]=bytes(sTok.encode('utf-8'))
+        currVocabSize=currVocabSize + 1
+
+
+    print('reading')
+    fileSize=os.path.getsize(input_path)
+
+    NUM_FILE_CHUNKS=4
+
+    chunkSize=int(np.ceil(float(fileSize)/NUM_FILE_CHUNKS))
+
+    def preTok(line,special_tokens,idx):
+        nonSpecialIndicesList=[]
+
+        allSpecialTokenRanges=getAllSpecialTokenLocs(line,special_tokens)
+
+        print('got special',datetime.datetime.now())
+
+        wordCount=dict()
+        wordToToks=dict()
+        pairToWord=dict()
+        tokPairCount=dict()
+
+        print('num nonspecial',len(allSpecialTokenRanges))
+
+
+        if (len(allSpecialTokenRanges)==0):
+            pretokenizeNonSpecial(line,
+                                  wordCount,
+                                  wordToToks,
+                                  pairToWord,
+                                  tokPairCount,
+                                  None)
+
+        else:
+            for rngIdx in range(len(allSpecialTokenRanges)):
+                if (rngIdx%10000==0):
+                    print(rngIdx)
+                rnge=allSpecialTokenRanges[rngIdx]
+                nonSpecialEnd=rnge[0]
+                if (rngIdx==0):
+                    nonSpecialStart=0
+                else:
+                    nonSpecialStart=allSpecialTokenRanges[rngIdx-1][1]
+
+                if (nonSpecialEnd > nonSpecialStart):
+                    pretokenizeNonSpecial(line[nonSpecialStart:nonSpecialEnd],
+                                          wordCount,
+                                          wordToToks,
+                                          pairToWord,
+                                          tokPairCount,
+                                          None)
+        result=dict()
+        result['wordCount']=wordCount
+        result['wordToToks']=wordToToks
+        result['pairToWord']=pairToWord
+        result['tokPairCount']=tokPairCount
+
+        pickle.dump(result,open('result_%d'%idx,'wb'))
+
+
+    resultList=[]
+    numFilled=0
+    with open(input_path,'rb') as f:
+        boundaries = find_chunk_boundaries(f, NUM_FILE_CHUNKS, b"<|endoftext|>")
+        print('boundaries are',boundaries)
+        processes=[]
+
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            print(start,end)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            proc=Process(target=preTok,args=(chunk,special_tokens,numFilled))
+            proc.start()
+            processes.append(proc)
+            #preTok(chunk,special_tokens,resultList[numFilled])                                                                                  
+            numFilled=numFilled+1
+        for pr in processes:
+            pr.join()
+
+            
+    for idx in range(numFilled):
+        resultList.append(pickle.load(open('result_%d'%idx,'rb')))
+    for r in resultList:
+        print('looking')
+        print([ky for ky in r])
+
+    res=mergePretokenized([r['wordCount'] for r in resultList],
+                      [r['wordToToks'] for r in resultList],
+                      [r['pairToWord'] for r in resultList],
+                      [r['tokPairCount'] for r in resultList])
+
+    wordCount=res['wordCount']
+    wordToToks=res['wordToToks']
+    pairToWord=res['pairToWord']
+    tokPairCount=res['tokPairCount']
+
+    print('pretokenized')
+    numMerges=vocab_size - currVocabSize
+
+    merges=[None]*numMerges
+
+    def show(x):
+        pair,cnt=x
+        print((vocab[pair[0]],vocab[pair[1]],cnt))
+
+    MINUS_LARGE=-100000
+
+    class TokPairItem(object):
+        def __init__(self,item):
+            self.item=item
+            self.vocab0=vocab[self.item[0][0]]
+            self.vocab1=vocab[self.item[0][1]]
+            self.count=item[1]
+
+        def __gt__(self,other):
+            #print('invoking')                                                                                                                   
+            if (self.item[1] > other.item[1]):
+                #print('used item[1]')                                                                                                           
+                return True
+            #print('tiebreaker')                                                                                                                 
+            return (self.vocab0,self.vocab1) > (other.vocab0,other.vocab1)
+
+    def tokCompare(one,other):
+        return (one.count > other.count) or ( (one.count==other.count) and ( (one.vocab0,one.vocab1) > (other.vocab0,other.vocab1) ) )
+
+    def make_comparator(greater_than):
+        def compare(x,y):
+            if (greater_than(x,y)):
+                return -1
+            elif (greater_than(y,x)):
+                return 1
+            else:
+                return 0
+        return compare
+
+
+    myKey=cmp_to_key(make_comparator(tokCompare))
+
+    tokPairItemList=sorted([TokPairItem(item) for item in tokPairCount.items()],key=myKey)
+
+    pairToItem=dict(zip([tpi.item[0] for tpi in tokPairItemList],tokPairItemList))
+
+
+    def gt(item1,item2):
+        if (item1[1]==item2[1]):
+            return item1[0] > item2[0]
+        return item1[1] > item2[1]
+
+
+    print('I made merges',datetime.datetime.now())
+
+    totalPairTime=0.
+    totalChangeTime=0.
+    for mIdx in range(numMerges):
+
+
+       topPairItem=tokPairItemList[0]
+
+       topPair=topPairItem.item[0]
+
+       if (tokPairCount[topPair]==0):
+           continue
+
+       del tokPairCount[topPair]
+       tokPairItemList=tokPairItemList[1:]
+
+       #tokPairItemList.remove(tokPairItemList[0])                                                                                               
+
+       #b1=vocab[topPair[0]]                                                                                                                     
+       #b2=vocab[topPair[1]]                                                                                                                     
+       newTok=currVocabSize
+       vocab[newTok]=topPairItem.vocab0 + topPairItem.vocab1#b1 + b2   
+       
+       merges[mIdx]=(topPairItem.vocab0,topPairItem.vocab1)
+       currVocabSize=currVocabSize + 1
+
+       wordsToUpdate=pairToWord[topPair]
+
+       changedTokPairs=set()
+
+       newPairs=[]
+
+       for wordToUpdate in wordsToUpdate:
+           
+           wordCountForWord=wordCount[wordToUpdate]
+
+           tokList=wordToToks[wordToUpdate]
+
+           newTokList=[]
+           tokListLen=len(tokList)
+
+           idx=0
+
+           while (idx < tokListLen):
+               if (idx < (tokListLen -1) ):
+                   if ((tokList[idx],tokList[idx+1])==topPair):
+                       newTokList.append(newTok)
+                       idx=idx + 2
+                   else:
+                       newTokList.append(tokList[idx])
+                       idx=idx + 1
+               else:
+                   newTokList.append(tokList[idx])
+                   idx=idx + 1
+
+           oldPairList=[pl for pl in zip(tokList[0:-1],tokList[1:])]
+           oldPairSet=set(oldPairList)
+           newPairList=[pl for pl in zip(newTokList[0:-1],newTokList[1:])]
+           newPairSet=set(newPairList)
+
+           oldNotNew=oldPairSet.difference(newPairSet)
+           if (topPair in oldNotNew):
+               oldNotNew.remove(topPair)
+           newNotOld=newPairSet.difference(oldPairSet)
+           both=oldPairSet.intersection(newPairSet)
+
+           
+           oldCount=collections.Counter(oldPairList)#count(oldPairList)                                                                         
+           newCount=collections.Counter(newPairList)#count(newPairList)                                                                          
+           for pair in oldNotNew:
+               #if (pair != topPair):                                                                                                            
+               changedTokPairs.add(pair)
+               tokPairCount[pair]=tokPairCount[pair] - oldCount[pair]*wordCountForWord
+
+           for pair in newNotOld:
+               if (pair in tokPairCount):
+                   if (pair in pairToItem):
+                       changedTokPairs.add(pair)
+               else:
+                   newPairs.append(pair)
+
+               tokPairCount[pair]=tokPairCount.get(pair,0) + newCount[pair]*wordCountForWord
+
+               if (pair in pairToWord):
+                   pairToWord[pair].add(wordToUpdate)
+               else:
+                   pairToWord[pair]=set([wordToUpdate])
+
+           for pair in both:
+               if (newCount[pair] != oldCount[pair]):
+                   #if (not (pair in changedTokPairs) ):                                                                                         
+                   changedTokPairs.add(pair)
+                   tokPairCount[pair]=tokPairCount[pair] + wordCountForWord*(newCount[pair] - oldCount[pair])
+
+           wordToToks[wordToUpdate]=newTokList
+
+       prePairTime=datetime.datetime.now()
+       for pair in changedTokPairs:
+           oldItem=pairToItem[pair]
+
+           tokPairItemList.remove(oldItem)
+           newItem=TokPairItem((pair,tokPairCount[pair]))
+           pairToItem[pair]=newItem
+           bisect.insort(tokPairItemList,newItem,key=myKey)#  cmp_to_key(make_comparator(tokCompare)))                                           
+
+       for pair in newPairs:
+           newItem=TokPairItem((pair,tokPairCount[pair]))
+           pairToItem[pair]=newItem
+           bisect.insort(tokPairItemList,newItem,key=myKey)
+
+    print('totalPairTime %4.4f'%totalPairTime)
+    print('totalChangeTime %4.4f'%totalChangeTime)
+    print('done',datetime.datetime.now())
+    return vocab,merges
